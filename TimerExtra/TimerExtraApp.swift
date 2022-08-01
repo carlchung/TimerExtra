cd6be4367e11cdf8dcbc89f2fd5caf9474183431
Carl Chung <carl.chung@gmail.com>
2022-08-01 12:40:46 +0100

    Initial Commit

---
+++ TimerExtra/TimerExtraApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerExtraApp.swift
+//  TimerExtra
+//
+//  Created by carl on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimerExtraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
